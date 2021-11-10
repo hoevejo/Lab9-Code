ea2d5483e87b3e891e4424bcd83104b2c5ac053c
Khawk2000 <70482761+Khawk2000@users.noreply.github.com>
2021-11-09 19:17:40 -0500

    Add files via upload

--- mm.c
+++ mm.c
@@ -8,7 +8,10 @@
 #include <math.h>
 
 
-int x, y, z, q, w;
+int x, y, z;
+int q = 100;
+int w = 200;
+int num[20];
 char buf[20];
 char *string;
 
@@ -20,44 +23,54 @@ int main(){
 
 	char r, t;
 	x=1;
-	y=2;
 	z=3;
-	int result = x+y;
-	int result2 = y+z;
+	int result = x+2;
+	int result2 = 2+z;
 	int final = result + result2;
 
 	double power = pow(2, final);
 	
-	r = calc(x,y);
-	t = calc(y,z);
-
+	r = calc(x,result);
+	t = calc(result2,z);
+	
+	int *numptr;
+	numptr = malloc(sizeof(num));
+	num[0] = x;
+	num[1] = y;
+	num[2] = z;
+	num[3] = r;
+	num[4] = t;
 	char buf2[30];
 	char *string2;
 	string = malloc(sizeof(buf));
 	string2 = malloc(sizeof(buf2));
+	printf("Enter two inputs for 2 different char arrays\n");
 	fgets(string, sizeof(string),stdin);
 	fgets(string2, sizeof(string2), stdin);
 //Print address of everything
-        printf("Q: %x ", &q);
-        printf("W: %x ", &w);
-        printf("R: %x ", &r);
-        printf("T: %x ", &t);
-        printf("X: %x ", &x);
-        printf("Y: %x ", &y);
-        printf("Z: %x ", &z);
-        printf("result1: %x ", &result);
-        printf("result2: %x ", &result2);
+        printf("Q: %x\n ", &q);
+        printf("W: %x\n ", &w);
+        printf("R: %x\n ", &r);
+        printf("T: %x\n ", &t);
+        printf("X: %x\n ", &x);
+        printf("Y: %x\n ", &y);
+        printf("Z: %x\n ", &z);
+        printf("result1: %x\n ", &result);
+        printf("result2: %x\n ", &result2);
         printf("final: %x\n", &final);
 	
 	printf("power: %x\n", &power);
 
-	printf("buf: %x ", &buf);
-	printf("string: %x ", string);
-	printf("buf2: %x ", &buf2);
+	printf("buf: %x\n ", &buf);
+	printf("string: %x\n ", string);
+	printf("buf2: %x\n ", &buf2);
 	printf("string2: %x\n", string2);
+	printf("num: %x\n", num);
+	printf("numptr: %x\n", numptr);
 
 	free(string);
 	free(string2);
+	free(numptr);
 	return 0;
 		
 }
